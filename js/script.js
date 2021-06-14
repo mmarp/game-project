ef197221a270effd117d6eee2ba73229a11799d4
Margarida Pereira <margarida.arq@gmail.com>
2021-06-14 10:58:37 +0100

    14/06/2021 - morning

--- js/script.js
+++ js/script.js
@@ -7,12 +7,12 @@ const context = canvas.getContext('2d');
 
 
 //1.Luis
-const luis = new Character('luis', './Images/c_margarida.jpg', 'male', 'straight', 'brown', true, 'brown', false, false);
+const luis = new Character('luis', './Images/margarida.jpg', 'male', 'straight', 'brown', true, 'brown', false, false);
 const luisPos = new Positioning(270, 75, 150, 150, luis);
 luisPos.draw();
 
 //2.Stefano
-const stefano = new Character('stefano','./Images/c_margarida.jpg', 'male', 'wavy', 'brown', true, 'blue', false, false);
+const stefano = new Character('stefano','./Images/margarida.jpg', 'male', 'wavy', 'brown', true, 'blue', false, false);
 const stefanoPos = new Positioning(440, 75, 150, 150, stefano);
 stefanoPos.draw();
 
@@ -27,32 +27,32 @@ const benPos = new Positioning(780, 75, 150, 150, ben);
 benPos.draw();
 
 //5.Joao F.
-const joaoF = new Character('joaoF','./Images/c_margarida.jpg', 'male', 'straight', 'black', true, 'brown', false, true);
+const joaoF = new Character('joaoF','./Images/margarida.jpg', 'male', 'straight', 'black', true, 'brown', false, true);
 const joaoFPos = new Positioning(270, 245, 150, 150, joaoF);
 joaoFPos.draw();
 
 //6.Tiago P.
-const tiagoP = new Character('tiagoP','./Images/c_margarida.jpg', 'male', 'straight', 'brown', true, 'green', false, false);
+const tiagoP = new Character('tiagoP','./Images/margarida.jpg', 'male', 'straight', 'brown', true, 'green', false, false);
 const tiagoPPos = new Positioning(440, 245, 150, 150, tiagoP);
 tiagoPPos.draw();
 
 //7.Gigi
-const gigi = new Character('gigi','./Images/c_margarida.jpg', 'female', 'straight', 'brown', true, 'brown', false, false);
+const gigi = new Character('gigi','./Images/gigi.jpg', 'female', 'straight', 'brown', true, 'brown', false, false);
 const gigiPos = new Positioning(610, 245, 150, 150, gigi);
 gigiPos.draw();
 
 //8.Joao B.
-const joaoB = new Character('joaoB','./Images/c_margarida.jpg', 'male', 'straight', 'brown', true, 'brown', false, false);
+const joaoB = new Character('joaoB','./Images/margarida.jpg', 'male', 'straight', 'brown', true, 'brown', false, false);
 const joaoBPos = new Positioning(780, 245, 150, 150, joaoB);
 joaoBPos.draw();
 
 //9.Victor
-const victor = new Character('victor','./Images/c_margarida.jpg', 'male', 'straight', 'black', false, 'brown', false, false);
+const victor = new Character('victor','./Images/margarida.jpg', 'male', 'straight', 'black', false, 'brown', false, false);
 const victorPos = new Positioning(270, 415, 150, 150, victor);
 victorPos.draw();
 
 //10.Tiago S.
-const tiagoS = new Character('tiagoS','./Images/c_margarida.jpg', 'male', 'straight', 'brown', true, 'brown', false, false);
+const tiagoS = new Character('tiagoS','./Images/margarida.jpg', 'male', 'straight', 'brown', true, 'brown', false, false);
 const tiagoSPos = new Positioning(440, 415, 150, 150, tiagoS);
 tiagoSPos.draw();
 
@@ -77,12 +77,12 @@ const helderPos = new Positioning(440, 585, 150, 150, helder);
 helderPos.draw();
 
 //15.Gabriel
-const gabriel = new Character('gabriel','./Images/helder_out.jpg', 'male', 'straight', 'brown', true, 'brown', false, false);
+const gabriel = new Character('gabriel','./Images/margarida.jpg', 'male', 'straight', 'brown', true, 'brown', false, false);
 const gabrielPos = new Positioning(610, 585, 150, 150, gabriel);
 gabrielPos.draw();
 
 //16.Miguel
-const miguel = new Character('miguel','./Images/c_margarida.jpg', 'male', 'curly', 'brown', true, 'brown', false, false);
+const miguel = new Character('miguel','./Images/miguel.jpg', 'male', 'curly', 'brown', true, 'brown', false, false);
 const miguelPos = new Positioning(780, 585, 150, 150, miguel);
 miguelPos.draw();
 
@@ -137,9 +137,11 @@ document.getElementById("start-button").onclick = () => {
   document.getElementById('f-prev-button').style.display = 'block';
   document.getElementById('f-answer').style.display = 'block';
   document.getElementById('f-next-button').style.display = 'block';
+  
 
   console.log(chosenCharacter);
 };
+
 
 
 
@@ -271,6 +273,9 @@ document.getElementById("ask-button").onclick = () => {
         span.innerText = 'NO';
         yesNo.appendChild(span);
     }
+
+    setTimeout( () => span.remove(), 2000);
+    //disabled button
     
 
 };
